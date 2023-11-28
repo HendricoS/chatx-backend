f3931fc25fc0942ba102b6d22fece600a6619ad6
Hendrico Stapelberg <hendrico.stapelberg@gmail.com>
2023-11-28 12:48:07 +0200

    updated

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,8 @@
 const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
+
 const User = require("../models/userModel");
 const Message = require("../models/messageModel");
 
